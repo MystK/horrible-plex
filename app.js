f5c7228d024cdc4fc7436386f4df748c80926cab
Antoine Pham <its@phamap.net>
2015-05-17 14:32:44 -0700

    Major restructure

--- app.js
+++ app.js
@@ -1,91 +1,80 @@
+/// <reference path="typings/node/node.d.ts"/>
 var fs = require('fs')
 var animePath = 'D:\\Media\\Anime\\'
 var downloadsPath = 'D:\\Downloads\\BooStudioLLC.TorrexPro_b6e429xa66pga!App\\Downloads\\Completed\\'
-var dPDA = fs.readdirSync(downloadsPath).filter(function (e) {return e.match(/\[HorribleSubs\].*\]\.mkv/)})
-var aPDA = fs.readdirSync(animePath).filter(function (e) {if (e.match(/\./)) return false;else return true})
+var dPDA = fs.readdirSync(downloadsPath).filter(function (e) {
+  return /\[HorribleSubs\].*\]\.mkv/.test(e)
+})
+var aPDA = fs.readdirSync(animePath).filter(function (e) {
+  return !/\./.test(e)
+})
 
-//Test stuff
-// console.log(process.argv[2]) //"[HorribleSubs] Shounen Hollywood - 13 [720p].mkv"
-// var fileName = "[HorribleSubs] Shounen Hollywood - 13 [720p].mkv"
-// console.log(process.argv[3]) //"D:\Downloads\BooStudioLLC.TorrexPro_b6e429xa66pga!App\Downloads\Completed"
-// console.log(fs.readdirSync(process.argv[3])) //D:\Downloads\BooStudioLLC.TorrexPro_b6e429xa66pga!App\Downloads\Completed
-//Test Functions
-// function createFolder(d) {console.log(d)}
-// function checkAll(d) {console.log('checking all')}
-var test
-function dummyCallbackCL(a,b) {console.log(a+': '+b)}
-function dummyCallbackDN() {}
-// function checkAll(d) {console.log('check all')}
-//----------------------------------------
+// First check which mode
+process.argv[2]? process.argv[2].match(/ - \d{2,} \[\d{3,}p].mkv/)?
+  checkOne(process.argv[2]): // DO SINGLE MODE
+  dPDA.length != 0? // DO NOTHING
+    checkAll() // DO ALL
+    : console.log('No new files')
+  : dPDA.length != 0? // DO NOTHING
+    checkAll() // DO ALL
+    : console.log('No new files')
 
-//First check which mode
-process.argv[2]?process.argv[2].match(/ - \d{2,} \[\d{3,}p].mkv/)?
-  checkOne(process.argv[2]): //DO SINGLE MODE
-  dPDA.length!=0? //DO NOTHING
-    checkAll() //DO ALL
-    :console.log('No new files')
-    :dPDA.length!=0? //DO NOTHING
-    checkAll() //DO ALL
-    :console.log('No new files')
-
-//Move just one anime file
-function checkOne(fileName) {
-  setTimeout(function () {
-    var animeName = fileName.slice(15,fileName.match(/ - \d{2,} \[\d{3,}p].mkv/).index)
-    checkForFolderOrCreate(animeName = fileName.slice(15,fileName.match(/ - \d{2,} \[\d{3,}p].mkv/).index), function(folderName) {
-      moveAnime(fileName, folderName)
-    })
-  },10000)
+// Move just one anime file
+function checkOne (fileName) {
+  var animeName = fileName.slice(15, fileName.match(/ - \d{2,} \[\d{3,}p].mkv/).index)
+  checkForFolderOrCreate(animeName = fileName.slice(15, fileName.match(/ - \d{2,} \[\d{3,}p].mkv/).index), function (folderName) {
+    moveAnime(fileName, folderName)
+  })
 }
-//----------------------------------------
+// ----------------------------------------
 
-//Move all anime files
-function checkAll() {
-  createGroupedDPDA(function(list) {
-    //Now check if folder is already made and make if not.
-    var d=0 //Used for line 59 of getting anime ID
-    for (i in list) {
-      checkForFolderOrCreate(i, function(folderName) {
-				for (var j in list[i]) {
-					moveAnime(list[i][j], folderName)
-				}
+// Move all anime files
+function checkAll () {
+  createGroupedDPDA(function (list) {
+    // Now check if folder is already made and make if not.
+    var d = 0 // Used for line 59 of getting anime ID
+    for (var i in list) {
+      checkForFolderOrCreate(i, function (folderName) {
+        for (var j in list[i]) {
+          moveAnime(list[i][j], folderName)
+        }
       })
     }
   })
 }
-//----------------------------------------
-//Group same anime first by creating new array. If no anime in downloads, start on IDs
-function createGroupedDPDA(callback) {
+// ----------------------------------------
+// Group same anime first by creating new array. If no anime in downloads, start on IDs
+function createGroupedDPDA (callback) {
   var groupedDPDA = []
   var i = 0
-  dPDA.forEach(function(e) {
-    var animeName = e.slice(15,e.match(/ - \d{2,} \[\d{3,}p].mkv/).index)
+  dPDA.forEach(function (e) {
+    var animeName = e.slice(15, e.match(/ - \d{2,} \[\d{3,}p].mkv/).index)
     if (! groupedDPDA[animeName]) {
-      groupedDPDA[animeName]=dPDA.filter(function (e) {return new RegExp(animeName).test(e)})
+      groupedDPDA[animeName] = dPDA.filter(function (e) {return new RegExp(animeName).test(e)})
     }
-    if (dPDA.length==++i) callback(groupedDPDA)
+    if (dPDA.length == ++i) callback(groupedDPDA)
   })
 }
-//----------------------------------------
-//Function to check for and/or create anime folder
-function checkForFolderOrCreate(name, callback) {
+// ----------------------------------------
+// Function to check for and/or create anime folder
+function checkForFolderOrCreate (name, callback) {
   var i = 0
   var folderName
   var nameSplit = name.split(' ')
   var nameToCheck = name
   while (! folderName) {
-		var possibleFolderNames = aPDA.filter(function (e) { return RegExp(nameToCheck, 'i').test(e) })
+    var possibleFolderNames = aPDA.filter(function (e) { return RegExp(nameToCheck, 'i').test(e) })
 
     switch (possibleFolderNames.length) {
       case 1:
         callback(folderName = possibleFolderNames[0])
         break
       case 0:
-      default: 
-        if (nameToCheck.match(/ \S*$/)) nameToCheck=nameToCheck.slice(0,nameToCheck.match(/ \S*$/).index)
-				else if (!possibleFolderNames.length) {
-					folderName = name
-          fs.mkdir(animePath+folderName,function () {  //Make the folder and move anime in callback
+      default:
+        if (nameToCheck.match(/ \S*$/)) nameToCheck = nameToCheck.slice(0, nameToCheck.match(/ \S*$/).index)
+        else if (!possibleFolderNames.length) {
+          folderName = name
+          fs.mkdir(animePath + folderName , function () {  // Make the folder and move anime in callback
             callback(folderName)
           })
         }
@@ -93,73 +82,66 @@ function checkForFolderOrCreate(name, callback) {
     }
   }
 }
-//----------------------------------------
+// ----------------------------------------
 
-//Function to move the files
-function moveAnime(name, folderName) {  //Make the folder and move anime in callback //test one
-	fs.rename(downloadsPath + name, animePath + folderName + '/' + name, function (err, data){
-		if (err) {
-			switch (err.errno) {
-				case -4048:
-					console.log(name , 'Already file with that name!');
-					break;
-				case -4082:
-					console.log(name , 'Program has exclusive access to file!');
-					break;
-				default:
-					console.log(err);
-			}
-		}
-	})
-  // if (++d==++groupedDPDA.length) startGettingAnimeID() //Mark for when making folder is done
+// Function to move the files
+var util = require('util')
+function moveAnime (name, folderName) {  // Make the folder and move anime in callback //test one
+  fs.rename(downloadsPath + name, animePath + folderName + '/' + name, function (err) {
+    if (err) {
+      switch (err.errno) {
+        case -4048:
+          console.error('Cannot move ' + name + ' to ' + folderName + '. Filename already exists there.')
+          break
+        case -4082: // Being used
+          setTimeout(function () {
+            moveAnime(name, folderName)
+          }, 500)
+          break
+        default:
+          console.log(err)
+      }
+    } else {
+      console.log('Moved ' + name + ' to ' + folderName)
+    }
+  })
+// if (++d==++groupedDPDA.length) startGettingAnimeID() //Mark for when making folder is done
 }
-//----------------------------------------
+// ----------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-//Functions to get anime ID from theTVdb
-function getIDfromXML(xml) {
-  id = xml.split('\n').filter(function (e) {return e.match(/<id>/)})
-  if (id.length!=1) return 'Unknown'
-  else return id[0].slice(4,id.length-6)
+// Functions to get anime ID from theTVdb
+function getIDfromXML (xml) {
+  var id = xml.split('\n').filter(function (e) {return e.match(/<id>/)})
+  if (id.length != 1) return 'Unknown'
+  else return id[0].slice(4, id.length - 6)
 }
- 
-function getAnimeID(folderName, callback) {
+
+function getAnimeID (folderName, callback) {
   var http = require('http')
   var xml = ''
-  http.get('http://thetvdb.com/api/GetSeries.php?seriesname='+folderName, function (res) {
+  http.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + folderName, function (res) {
     res.on('data', function (data) {
-      xml=xml.concat(data)
+      xml = xml.concat(data)
     })
     res.on('end', function () {
       callback(getIDfromXML(xml))
-      fs.writeFileSync(animePath+folderName+'/raw.xml',xml)
+      fs.writeFileSync(animePath + folderName + '/raw.xml', xml)
     })
   })
 }
 
-function startGettingAnimeID() {
+function startGettingAnimeID () {
   aPDA.forEach(function (e) {
-    if (fs.readdirSync(animePath+e).filter(function (e) {return e=='id'})=='') {
+    if (fs.readdirSync(animePath + e).filter(function (e) {return e == 'id'}) == '') {
       getAnimeID(e, function (data) {
-        fs.writeFileSync(animePath+e+'/id',data)
+        fs.writeFileSync(animePath + e + '/id', data)
       })
       console.log('No ID found')
     }
   })
 }
 
-//New one in progress
+// New one in progress
 /* function startGettingAnimeID() {
   aPDA.forEach(function (e) {
     if (fs.readdirSync(animePath+e).filter(function (e) {return ! /\.mkv/.test(e)}).length!=2) {
@@ -173,22 +155,4 @@ function startGettingAnimeID() {
 
 fs.readdirSync(animePath+e).filter(function (e) {return ! /\.mkv/.test(e)}).length!=2 */
 
-//------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ------------------------------------
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-//gulpfile.js
-var gulp = require('gulp');
-var livereload = require( 'gulp-livereload' );
-var supervisor = require( 'gulp-supervisor' );
-var mocha = require( 'gulp-mocha' );
+// gulpfile.js
+var gulp = require('gulp')
+var livereload = require('gulp-livereload')
+var supervisor = require('gulp-supervisor')
+var mocha = require('gulp-mocha')
 
-gulp.task('supervisor', function() {
-	supervisor( require('./package.json').scripts.backend, {
-		//ignore: [ ".idea", ".git","gulpfile.js"],
-		watch:['backend'],
-		exec: "iojs",
-		debug: true,
-		noRestartOn: "error"
-	} );
-} );
+gulp.task('supervisor', function () {
+  supervisor(require('./package.json').scripts.backend, {
+    // ignore: [ ".idea", ".git","gulpfile.js"],
+    watch: ['backend'],
+    exec: 'iojs',
+    debug: true,
+    noRestartOn: 'error'
+  })
+})
 
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch(['frontend/**/*', 'backend/**/*']).on('change', function(filePath){
-		setTimeout(function() {livereload.changed(filePath)},1000);
-	});
-	gulp.watch(['frontend/**/*', 'backend/**/*', 'tests/*_mocha.js'], ['mocha'])
-});
+gulp.task('watch', function () {
+  livereload.listen()
+  gulp.watch(['frontend/**/*', 'backend/**/*']).on('change', function (filePath) {
+    setTimeout(function () {livereload.changed(filePath)}, 1000)
+  })
+  gulp.watch(['frontend/**/*', 'backend/**/*', 'tests/*_mocha.js'], ['mocha'])
+})
 
-gulp.task('mocha', function() {
-	gulp.src('tests/*_mocha.js', {read: false})
-		.pipe(mocha())
-			.on('error', console.log);
-});
+gulp.task('mocha', function () {
+  gulp.src('tests/*_mocha.js', {read: false})
+    .pipe(mocha())
+    .on('error', console.log)
+})
 
 gulp.task('scratch', function () {
-	supervisor('scratch/generator scratch.js', {
-		watch: ['scratch/generator scratch.js'],
-		exec: "iojs",
-		debug: true,
-		noRestartOn: "exit"
-	});
-});
+  supervisor('scratch/generator scratch.js', {
+    watch: ['scratch/generator scratch.js'],
+    exec: 'iojs',
+    debug: true,
+    noRestartOn: 'exit'
+  })
+})
 
-gulp.task('default',['mocha', 'watch']);
+gulp.task('default', ['mocha', 'watch'])
